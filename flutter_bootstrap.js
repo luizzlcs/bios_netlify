17e45116296e6914e72751ab6c9dc13a1d53f7a4
Luiz Carlos <luizzlcs@gmail.com>
2025-01-31 11:37:00 -0300

    Removido link do retiro

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4135178887"
+    serviceWorkerVersion: "4019622911"
   }
 });
